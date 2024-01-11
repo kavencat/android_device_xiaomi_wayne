614eac0b51e311618ba6a028a62b076d29bd8fe6
kavencat <shi772425803@163.com>
2024-01-11 21:26:02 +0800

    wayne: add Initial superior
    Change-Id: I3bc79b1bf247d577f168f811ffd6b445a3d7f62b
    Signed-off-by: kavencat <shi772425803@163.com>

---
+++ superior_wayne.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from wayne device
+$(call inherit-product, device/xiaomi/wayne/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := superior_wayne
+PRODUCT_DEVICE := wayne
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 6X
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# GApps
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_STOCK_ARCORE := true
+BUILD_WITH_GAPPS := true
+
+# superior
+#SUPERIOR_OFFICIAL := true
+SUPERIOR_TEST := true
+
+BUILD_FINGERPRINT := xiaomi/wayne/wayne:9/PKQ1.180904.001/V12.0.2.0.PDCCNXM:user/release-keys
