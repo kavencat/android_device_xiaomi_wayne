f6816d28523a00bd8a3730d05a04abc839bd9a0c
micky387 <mickaelsaibi@free.fr>
2024-07-04 20:51:08 -0400

    wayne: Build libstagefright_softomx_plugin libraries
    E vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found.
    
    Change-Id: I4b21476fc04df2acc89f2a34035b1642f2176f7f
    Signed-off-by: kavencat <shi772425803@163.com>

--- device.mk
+++ device.mk
@@ -279,7 +279,8 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.0.vendor
+    android.hardware.media.c2@1.0.vendor \
+    libstagefright_softomx_plugin.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
