57b755c776f2a88d2f53c8982a9d16c605c43d7e
kavencat <shi772425803@163.com>
2024-10-24 20:18:09 +0800

    wayne; test
    Change-Id: Id8914ef46415f3dffbb1e0620ba1eee9cd593bf4
    Signed-off-by: kavencat <shi772425803@163.com>

--- device.mk
+++ device.mk
@@ -81,10 +81,10 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    android.hardware.btconfigstore@1.0.vendor \
+    android.hardware.btconfigstore@2.0.vendor \
     android.hardware.bluetooth.audio-impl
 
 PRODUCT_COPY_FILES += \
